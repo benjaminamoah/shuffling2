bc3bc0f9b02099d60e84203c62782478d01bef6d
Benjamin Amoah <33426921+benjaminamoah@users.noreply.github.com>
2019-08-19 08:49:25 -0400

    Add files via upload

---
+++ precache-manifest.76dca443c67365e9bb5f00c5cf0dc773.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed0899f9596bdce15da56791d82c72af",
+    "url": "/shuffling2/index.html"
+  },
+  {
+    "revision": "97c1bddfe4e313950064",
+    "url": "/shuffling2/static/css/main.89ffe34c.chunk.css"
+  },
+  {
+    "revision": "c79f2ab33b1afe0fe496",
+    "url": "/shuffling2/static/js/2.6d328d6d.chunk.js"
+  },
+  {
+    "revision": "97c1bddfe4e313950064",
+    "url": "/shuffling2/static/js/main.d422645c.chunk.js"
+  },
+  {
+    "revision": "a130ee94af171e8982d5",
+    "url": "/shuffling2/static/js/runtime~main.a8a8fa16.js"
+  }
+]);
